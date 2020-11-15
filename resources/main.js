e6223d85a32663cd267c5198960074a1c838455c
Roxana Stan <stan.roxana88@gmail.com>
2020-11-15 18:15:10 +0200

    Update main.js

--- resources/main.js
+++ resources/main.js
@@ -1,38 +1,39 @@
 $(function() { 
   const galleryCoats=$(".gallery-coats");
-  getpropertyHTML = function(propertyObj) {
-    return `<div class="box-1">
-        <div class="icon-coat" style="background-image:url(assets/coats/${propertyObj.imgUrl})"></div>
+  getproductsHTML = function(productsObj) {
+    return `<div class="box-1" data-id=${productsObj.id}>
+        <div class="icon-coat" style="background-image:url(assets/coats/${productsObj.imgUrl})"></div>
         <div class="wrapper-text-box">
-             <div>${propertyObj.name}</div>
-             <div>${propertyObj.currency} ${propertyObj.price}</div>
+             <div>${productsObj.name}</div>
+             <div>${productsObj.currency} ${productsObj.price}</div>
         </div>
         </div>`;
   };
 
   for(let i = 0; i < products.coats.length; i++) {
-    // aici nu ai properties, ai products, atentie la denumiri
-    let propertyObj = products.coats[i];
-        propertyHMTL = getpropertyHTML(propertyObj);
-        galleryCoats.append(propertyHMTL);
+    let productsObj = products.coats[i];
+        productsHTML = getproductsHTML(productsObj);
+        galleryCoats.append(productsHTML);
 
   };
+
+  galleryCoats.delegate(".icon-coat", "click", function(){
+    $(".overlay").show();
+
+    const id = $(this).parents(".box-1").data("id");
+        for(let i=0; i < products.coats.length; i++){
+            let changedObj = products.coats[i];
+            if(changedObj.id === id){
+                $(".image-overlay").css("background-image",`url(assets/coats/${changedObj.imgUrl})`);
+                $(".name-overlay").text(`${changedObj.name}`);
+                $(".price-overlay").text(`${changedObj.currency}${changedObj.price}`);
+                $(".composition-overlay").text(`${changedObj.composition}`);
+                $(".country-overlay").text(`${changedObj.country}`);
+                $(".care-overlay").text(`${changedObj.care}`);
+            };
+          };
 });
-
-// javascript aici? pentru un eventual interviu daca ti se cere jQuery, iti recomand sa scrii jQuery. Daca nu esti sigura, atunci poate ar trebui sa optezi doar pentru javascript peste tot
-
-// const wrapperOverlay=document.getElementsById("wrapperoverlay");
-// const buttonOpen=document.getElementById("button-open");
-// const buttonClose=document.getElementById("button-close");
-
-// buttonOpen.addEventListener("click", function() {
-//   wrapperOverlay.classList.add("show-overlay");
-// }
-// );
-
-// buttonClose.addEventListener("click", function() {
-//   wrapperOverlay.classList.remove("show-overlay");
-// }
-// );
-
-// ca recomandari generale, pentru un eventual interviu te sfatuiesc sa pui accent pe click event pe produce cu event delegation, pe click pe icon de close sa inchizi overlay-ul, se primesc puncte frumoase pe flow
+    $('.icon-close').click(function(){
+    $(".overlay").hide();
+});
+});
